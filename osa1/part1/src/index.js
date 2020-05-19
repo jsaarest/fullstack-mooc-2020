d8dc98ed07deb492f42ebd32489a894dc194fa75
jsaarest <39669220+jsaarest@users.noreply.github.com>
2020-05-19 21:29:33 +0300

    destructuring

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = ({name, age}) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born {bornYear()}</p>
+    </div>
+  )
+};
+
+const App = () => {
+  const nimi = 'Pekka';
+  const ika = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
