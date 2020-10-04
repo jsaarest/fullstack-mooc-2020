b079b58910cc805e8fb7ef3aa3074528828daa74
jsaarest <39669220+jsaarest@users.noreply.github.com>
2020-10-04 19:42:03 +0300

    Unicafe tehtävä

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,29 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Hello = ({name, age}) => {
 
-  const bornYear = () => new Date().getFullYear() - age;
 
-  return (
+const StatisticsLine = ({text, value}) => {
+  return(
     <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born {bornYear()}</p>
+      <p>{text} : {value}</p>
     </div>
   )
-};
+}
+const Statistics = ({data}) => {
+
+  return(
+    <>
+    <StatisticsLine text="Good" value={data.good}/>
+    <StatisticsLine text="Neutral" value={data.neutral}/>
+    <StatisticsLine text="Bad" value={data.bad}/>
+    <StatisticsLine text="All" value={data.bad + data.good + data.neutral}/>
+    <StatisticsLine text="Average" value={(data.bad + data.good + data.neutral / 3).toFixed(2)}/>
+    <StatisticsLine text="Positive" value={(data.good / (data.good + data.bad + data.neutral) * 100).toFixed(2) + "%" }/>
+    </>
+  )
+}
+
+
+
+const defaultData = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
 
 const App = () => {
-  const nimi = 'Pekka';
-  const ika = 10;
+  // tallenna napit omaan tilaansa
+  const [data, setData] = useState(defaultData)
+  console.log(data)
+
+  const Increase = (e) => {
+
+    const {name, value} = e.target
+  
+    setData({...data, [name]: parseInt(value)  + 1})
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+
+      <h1>Give feedback</h1>
+
+      <div>
+        <button name="good" value={data.good} onClick={Increase}>Good</button>
+        <button name="neutral" value={data.neutral} onClick={Increase}>Neutral</button>
+        <button name="bad" value={data.bad} onClick={Increase}>Bad</button>
+      </div>
+
+      <h2>Statistics</h2>
+
+      <Statistics data={data}/>
+
     </div>
   )
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
